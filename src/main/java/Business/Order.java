ee7699f866e95123c711231eebdb95573e801777
LiaBebop <128011196+LiaBebop@users.noreply.github.com>
2024-01-28 10:14:36 -0500

    JSP's added
    Added various JSP's

--- src/main/java/Business/Order.java
+++ src/main/java/Business/Order.java
@@ -1,4 +1,6 @@
 package Business;
 
+//Similar to the customer class, the employee object will generate a unique primary id automatically in the database when a new employee is inserted
+//View the insertDB() method in the customer class for example
 public class Order {
 }
