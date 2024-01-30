206d3f5d1da56917d33c5ee301590b48e568a9b4
LiaBebop <128011196+LiaBebop@users.noreply.github.com>
2024-01-30 17:53:47 -0500

    Updated Properties for Part.java

--- src/main/java/Business/Part.java
+++ src/main/java/Business/Part.java
@@ -8,11 +8,18 @@ import java.sql.ResultSet;
 public class Part {
     private String productID;
     private String productType;
+    private String brand;
     private String name;
     private String description;
     private double price;
     private int stockQuantity;
+    private boolean fitsSUV;
+    private boolean fitsSedan;
+    private boolean fitsTruck;
+    private boolean fitsCoupe;
+    private boolean fitsSport;
 
+    //<editor-fold desc="Get/Set Functions">
     public String getProductID() {
         return productID;
     }
@@ -27,6 +34,14 @@ public class Part {
 
     public void setProductType(String productType) {
         this.productType = productType;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
     }
 
     public String getName() {
@@ -57,34 +72,82 @@ public class Part {
         return stockQuantity;
     }
 
-    public void setStockQu  antity(int stockQuantity) {
+    public void setStockQuantity(int stockQuantity) {
         this.stockQuantity = stockQuantity;
     }
 
-    //<editor-fold desc="Database Functions">
+    public boolean getFitsSUV() {
+        return fitsSUV;
+    }
+
+    public void setFitsSUV(boolean fitsSUV) {
+        this.fitsSUV = fitsSUV;
+    }
+
+    public boolean getFitsSedan() {
+        return fitsSedan;
+    }
+
+    public void setFitsSedan(boolean fitsSedan) {
+        this.fitsSedan = fitsSedan;
+    }
+
+    public boolean getFitsTruck() {
+        return fitsTruck;
+    }
+
+    public void setFitsTruck(boolean fitsTruck) {
+        this.fitsTruck = fitsTruck;
+    }
+
+    public boolean getFitsCoupe() {
+        return fitsCoupe;
+    }
+
+    public void setFitsCoupe(boolean fitsCoupe) {
+        this.fitsCoupe = fitsCoupe;
+    }
+
+    public boolean getFitsSport() {
+        return fitsSport;
+    }
+
+    public void setFitsSport(boolean fitsSport) {
+        this.fitsSport = fitsSport;
+    }
+
+    //</editor-fold>
+
+//<editor-fold desc="Database Functions">
 
     /***********************************************************************************
      *  selectDB() assigns the associated Customer information from the database to the object
      ***********************************************************************************/
-    public boolean selectDB(int customerID) {
+    public boolean selectDB(String productID) {
         try {
-
             Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
             Connection connection = DriverManager.getConnection("jdbc:ucanaccess://Database/eCommerceDB.accdb");
 
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM Customers WHERE CustomerID=?");
-            statement.setInt(1, customerID);
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM Products WHERE ProductID=?");
+            statement.setString(1, productID);
 
             ResultSet resultSet;
             resultSet = statement.executeQuery();
             resultSet.next();
 
-            setCustomerID(resultSet.getInt("CustomerID"));
-            setFirstName(resultSet.getString("FirstName"));
-            setLastName(resultSet.getString("LastName"));
-            setAddress(resultSet.getString("Address"));
-            setPassword(resultSet.getString("password"));
-            setEmail(resultSet.getString("email"));
+            setProductID(resultSet.getString("ProductID"));
+            setProductType(resultSet.getString("ProductType"));
+            setBrand(resultSet.getString("Brand"));
+            setName(resultSet.getString("ProductName"));
+            setDescription(resultSet.getString("Description"));
+            setPrice(resultSet.getDouble("Price"));
+            setStockQuantity(resultSet.getInt("Stock"));
+            setFitsSUV(resultSet.getBoolean("FitsSUV"));
+            setFitsSedan(resultSet.getBoolean("FitsSedan"));
+            setFitsTruck(resultSet.getBoolean("FitsTruck"));
+            setFitsCoupe(resultSet.getBoolean("FitsCoupe"));
+            setFitsSport(resultSet.getBoolean("FitsSport"));
+
 
             connection.close();
             return true;
@@ -94,26 +157,28 @@ public class Part {
 
     /***********************************************************************************
      *  insertDB() inserts the Customer information into the Database
-     *  Since the customerID is auto-generated in the database no customerID is used
      ***********************************************************************************/
     public boolean insertDB() {
         try  {
             Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
             Connection connection = DriverManager.getConnection("jdbc:ucanaccess://Database/eCommerceDB.accdb");
 
-            PreparedStatement statement = connection.prepareStatement("INSERT INTO Customers ([FirstName], [LastName], [email], [Address], [Password]) VALUES (?,?,?,?,?)");
-            statement.setString(1, getFirstName());
-            statement.setString(2, getLastName());
-            statement.setString(3, getEmail());
-            statement.setString(4, getAddress());
-            statement.setString(5, getPassword());
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO Products ([ProductID], [ProductType], [Brand], [ProductName], [Description], [Price], [Stock], [FitsSUV], [FitsSedan], [FitsTruck], [FitsCoupe], [FitsSport]) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
+            statement.setString(1, getProductID());
+            statement.setString(2, getProductType());
+            statement.setString(3, getBrand());
+            statement.setString(4, getName());
+            statement.setString(5,getDescription());
+            statement.setDouble(6, getPrice());
+            statement.setInt(7, getStockQuantity());
+            statement.setBoolean(8, getFitsSUV());
+            statement.setBoolean(9, getFitsSedan());
+            statement.setBoolean(10, getFitsTruck());
+            statement.setBoolean(11, getFitsCoupe());
+            statement.setBoolean(12, getFitsSport());
 
             statement.executeUpdate();
 
-            //The auto-generated key is returned and set using the getGeneratedKeys() function
-            ResultSet resultSet = statement.getGeneratedKeys();
-            resultSet.next();
-            setCustomerID(resultSet.getInt(1));
             connection.close();
 
             return true;
@@ -129,13 +194,20 @@ public class Part {
             Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
             Connection connection = DriverManager.getConnection("jdbc:ucanaccess://Database/eCommerceDB.accdb");
 
-            PreparedStatement statement = connection.prepareStatement("UPDATE Customers SET [FirstName] = ?, [LastName] = ?, [email] = ?, [Address] = ?, [Password] = ? WHERE CustomerID = ?");
-            statement.setString(1, getFirstName());
-            statement.setString(2, getLastName());
-            statement.setString(3, getEmail());
-            statement.setString(4, getAddress());
-            statement.setString(5, getPassword());
-            statement.setInt(6, getCustomerID());
+            PreparedStatement statement = connection.prepareStatement("UPDATE Products SET [ProductType] = ?, [Brand] = ?, [ProductName] = ?, [Description] = ?, [Price] = ?, [Stock] = ?, [FitsSUV] = ?,[FitsSedan] = ?,[FitsTruck] = ?,[FitsCoupe] = ?,[FitsSport] = ? WHERE CustomerID = ?");
+
+            statement.setString(1, getProductType());
+            statement.setString(2, getBrand());
+            statement.setString(3, getName());
+            statement.setString(4,getDescription());
+            statement.setDouble(5, getPrice());
+            statement.setInt(6, getStockQuantity());
+            statement.setBoolean(7, getFitsSUV());
+            statement.setBoolean(8, getFitsSedan());
+            statement.setBoolean(9, getFitsTruck());
+            statement.setBoolean(10, getFitsCoupe());
+            statement.setBoolean(11, getFitsSport());
+            statement.setString(12, getProductID());
 
             statement.executeUpdate();
             connection.close();
@@ -152,8 +224,8 @@ public class Part {
             Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
             Connection connection = DriverManager.getConnection("jdbc:ucanaccess://Database/eCommerceDB.accdb");
 
-            PreparedStatement statement = connection.prepareStatement("DELETE FROM Customers WHERE CustomerID = ?");
-            statement.setInt(1, getCustomerID());
+            PreparedStatement statement = connection.prepareStatement("DELETE FROM Products WHERE ProductID = ?");
+            statement.setString(1, getProductID());
 
             statement.executeUpdate();
             connection.close();
@@ -162,5 +234,12 @@ public class Part {
         } catch (Exception ex) {System.out.println(ex); return false;}
     }
 
+
     //</editor-fold>
+
+    public static void main(String[] args){
+
+
+
+    }
 }
