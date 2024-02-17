03bb9690fdb30ee33c21e751581d861cc580df8f
Zenogr <132322726+Zenogr@users.noreply.github.com>
2024-02-16 20:11:06 -0500

    FootUpdate

---
+++ src/main/webapp/WEB-INF/Padfeet.js
@@ -0,0 +1,9 @@
+fetch('footer.html')
+
+  .then(res => res.text())
+  .then(text => {
+    let oldElement = document.querySelector("script#add_the_feet");
+    let newElement = document.createElement("div");
+    newElement.innerHTML = text;
+    oldElement.parentNode.replaceChild(newElement,oldElement);
+  })
